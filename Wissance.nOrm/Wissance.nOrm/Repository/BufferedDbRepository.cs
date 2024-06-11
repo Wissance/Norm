ce66456740bd630676db62e1d1e43a2ee1e82f7c
Ushakov Michale <um.nix.user@gmail.com>
2024-06-12 00:57:06 +0500

    buffered db repo class (empty)

---
+++ Wissance.nOrm/Wissance.nOrm/Repository/BufferedDbRepository.cs
@@ -0,0 +1,49 @@
+using Wissance.nOrm.Entity;
+
+namespace Wissance.nOrm.Repository
+{
+    public class BufferedDbRepository<T, TId> : IDbRepository<T, TId>
+        where T : class, IDbEntity<TId>, new()
+        where TId : IComparable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetManyAsync(IDictionary<string, object> whereClause, int? page, int? size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetOneAsync(IDictionary<string, object> whereClause)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertAsync(T item, bool immediately)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> BulkInsertAsync(IList<T> items, bool immediately)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T item, bool immediately)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> BulkUpdateAsync(IList<T> items, bool immediately)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SyncAsync(int[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
