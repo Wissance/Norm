e1fe99a6e5ccec8240c2bc822d1703d65cf2351d
Ushakov Michale <um.nix.user@gmail.com>
2025-04-02 17:35:02 +0500

    structure sql was configured

--- Wissance.nOrm/Wissance.nOrm.MySql.Tests/TestData/test_db_structure.sql
+++ Wissance.nOrm/Wissance.nOrm.MySql.Tests/TestData/test_db_structure.sql
@@ -13,7 +13,7 @@ CREATE TABLE `measure_units` (
    `description` NVARCHAR(256) NULL,
    `physical_value_id` INT NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT `fk_physical_value_id` FOREIGN KEY (`physical_value_id`)
+    CONSTRAINT `fk_measure_units_physical_value_id` FOREIGN KEY (`physical_value_id`)
     REFERENCES `physical_values` (`id`)
     ON DELETE CASCADE 
     ON UPDATE CASCADE
@@ -22,11 +22,38 @@ CREATE TABLE `measure_units` (
 CREATE TABLE `parameters` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` NVARCHAR(256) NOT NULL,
+    `type` NVARCHAR(32) NOT NULL,
     `aliases` NVARCHAR(512) NULL DEFAULT '',
     `description` NVARCHAR(512) NULL DEFAULT '',
     `measure_unit_id` INT NOT NULL,
-    CONSTRAINT `fk_measure_unit_id` FOREIGN KEY (`measure_unit_id`)
+    `update_frequency` INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_parameters_measure_unit_id` FOREIGN KEY (`measure_unit_id`)
     REFERENCES `measure_units` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
-) 
+);
+
+CREATE TABLE `parameters_values` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `value` TEXT NOT NULL,
+    `time` TIMESTAMP NOT NULL,
+    `parameter_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_parameters_values_parameter_id` FOREIGN KEY (`parameter_id`)
+    REFERENCES `parameters` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE `parameters_time_series_values` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `value` TEXT NOT NULL,
+    `time` TIMESTAMP NOT NULL,
+    `parameter_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_parameters_time_series_values_parameter_id` FOREIGN KEY (`parameter_id`)
+    REFERENCES `parameters` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
