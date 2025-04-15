e9ceeb425caa45cb8dd26b55d7e81a7b8c20cc94
Ushakov Michale <um.nix.user@gmail.com>
2025-04-16 00:24:42 +0500

    Postgres project

--- Wissance.nOrm/Wissance.nOrm.Postgres/Repository/PostgresBufferedRepository.cs
+++ Wissance.nOrm/Wissance.nOrm.Postgres/Repository/PostgresBufferedRepository.cs
@@ -1,7 +1,15 @@
+using Microsoft.Extensions.Logging;
+using Wissance.nOrm.Entity.QueryBuilders;
+using Wissance.nOrm.Repository;
+
 namespace Wissance.nOrm.Postgres.Repository
 {
-    public class PostgresBufferedRepository
+    public class PostgresBufferedRepository<T>: BufferedDbRepository<T>
+        where T: class, new()
     {
-
+        public PostgresBufferedRepository(string connStr, int bufferThreshold, IDbEntityQueryBuilder<T> sqlBuilder, Func<object[], IList<string>, T> entityFactoryFunc, ILoggerFactory loggerFactory) 
+            : base(connStr,bufferThreshold, new PostgresAdapter(), sqlBuilder, entityFactoryFunc, loggerFactory)
+        {
+        }
     }
 }
